1dce2dba41dc2f331178e2badb0c16c7fa96a5a0
Bhavya <bhavyanagasreeyamala@gmail.com>
2026-06-11 22:56:07 +0530

    PDF extraction and text cleaning completed

---
+++ src/pdf_processor.py
@@ -0,0 +1,22 @@
+import fitz
+from text_cleaner import clean_text
+
+def extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+
+    text = ""
+
+    for page in doc:
+        text += page.get_text()
+
+    doc.close()
+
+    return text
+
+pdf_path = "../data/sample_paper.pdf"
+
+raw_text = extract_text_from_pdf(pdf_path)
+
+cleaned_text = clean_text(raw_text)
+
+print(cleaned_text[:3000])
