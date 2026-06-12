744404040eab8d9108b0cdc566502944af5a608e
Bhavya <bhavyanagasreeyamala@gmail.com>
2026-06-12 21:07:50 +0530

    Initial project setup

--- src/text_cleaner.py
+++ src/text_cleaner.py
@@ -2,13 +2,8 @@ import re
 
 def clean_text(text):
 
-    # Remove multiple spaces
     text = re.sub(r'\s+', ' ', text)
-
-    # Remove citations like [1], [25]
     text = re.sub(r'\[\d+\]', '', text)
-
-    # Remove extra special characters
     text = re.sub(r'[^a-zA-Z0-9.,!?():;\-\s]', '', text)
 
     return text.strip()
